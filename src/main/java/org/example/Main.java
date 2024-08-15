cd8ee56b0ddf5d08e1c2e59e27a4ff3189b40433
Aldita Nurrahma Diani <aldita.diani@finaccel.co>
2024-08-15 14:26:19 +0700

    tambah looping

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Test");
+        System.out.println("Hello juga!");
+
+        // Test lagi
+        for (int i = 2; i <= 10; i++){
+            System.out.println(i);
+        }
     }
 }
